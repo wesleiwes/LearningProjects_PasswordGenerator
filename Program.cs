ded0ed7be92fb04e7ab36ca0bc298116a8eb4233
Weslei Rodrigues <wesleiralves@gmail.com>
2024-01-13 15:44:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace PasswordGeneration
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com a quantidade de caracteres para sua senha ser gerada: ");
+            int passwordLength = int.Parse(Console.ReadLine());
+            string password = PassRandon(passwordLength);
+            Console.WriteLine("SENHA GERADA: ");
+            Console.Write(password);
+          
+            
+        }
+        static string PassRandon(int size)
+        {
+            var capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ";
+            var smallLetter = capitalLetter.ToLower();
+            var number = "123456789";
+            var symbols = "*-+,;:.!@#$%&()-_=+";
+            string finalPass = (number + smallLetter + capitalLetter + symbols) ;
+            
+            Random randPass = new Random();
+            char[] pass = new char [size];
+            for (int i=0;i<size;i++)
+            {
+                int aleatory = randPass.Next(finalPass.Length);
+                pass[i] = finalPass[aleatory];
+            }
+            return new string(pass);
+            
+            
+        }
+       
+    }
+}
